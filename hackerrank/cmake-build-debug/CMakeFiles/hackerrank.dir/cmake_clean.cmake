9690a0d6e530eae491ef2a60e158bceb2502b226
bormaa <e.emil@nu.edu.eg>
2020-04-06 23:59:28 +0200

    New project

---
+++ hackerrank/cmake-build-debug/CMakeFiles/hackerrank.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hackerrank.dir/main.cpp.o"
+  "hackerrank"
+  "hackerrank.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hackerrank.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
